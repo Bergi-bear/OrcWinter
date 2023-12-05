d4092241899b113f83ab1fbc982f2366a54961bb
Bergi-bear <tarantyl@inbox.ru>
2023-12-05 16:09:29 +0300

    Альфарелиз

--- build.lua
+++ build.lua
@@ -1,6 +1,6 @@
 require 'build'({
-    run = 'editor',
+    --run = 'editor',
     --run  = 'game',
 })
---os.execute("start mpqtool new ./map.w3x OrcWinter.w3x")
+os.execute("start mpqtool new ./map.w3x OrcWinter.w3x")
 
